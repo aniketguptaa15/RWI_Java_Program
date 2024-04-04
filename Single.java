61a55736be5a466ae93083578592c39a62cb1c48
Aniket Gupta <aniketguptaa15@gmail.com>
2024-04-05 01:56:20 +0530

    Inheritance program updated

---
+++ Single.java
@@ -0,0 +1,12 @@
+ class Dog {
+    void eat(){
+        System.out.println("Dog is eating");
+    }
+}
+class  Single extends  Dog{
+    public static void main (String args[]){
+    Single doing =new Single();
+    doing.eat();
+
+    }
+}
