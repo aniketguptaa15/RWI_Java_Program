c8ef84a1cbaf46deea33f1546092067114725831
Aniket Gupta <aniketguptaa15@gmail.com>
2024-04-06 02:37:51 +0530

    Code Updated

--- Single.java
+++ Single.java
@@ -1,4 +1,4 @@
- class Dog {
+class Dog {
     void eat(){
         System.out.println("Dog is eating");
     }
---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    Test(){
+        System.out.println("This is in constructor");
+    }
+    public static void main(String args[]){
+     Test t=new Test();
+    }
+}
